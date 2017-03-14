990145f8a925da114ecc24f134330f14e54e0758
Lawrence Romano <l.j.romano@gmail.com>
2017-03-14 11:48:40 -0400

    Added 4 SELECT queries for Exer. 2

--- student-exercises/romano-select.sql
+++ student-exercises/romano-select.sql
@@ -8,8 +8,18 @@ SELECT * FROM individual WHERE deceasedDate IS NULL;
 SELECT DISTINCT genre FROM band;
 
 SELECT * FROM band WHERE genre = 'Country' ORDER BY yearFormed DESC;
-*/
 
+-------EXC. 1
 SHOW Columns from band;
 
 SELECT ID, name, genre from band;
+
+-------EXC. 2
+*/
+SELECT * FROM individual WHERE LastName = 'Jennings';
+
+SELECT ID, lastName FROM individual WHERE deceasedDate IS NOT NULL;
+
+SELECT ID, lastName, BirthDate FROM individual WHERE year(birthDate) > 1940;
+
+SELECT * FROM individual WHERE ID IN (1,3,5,7,19);
