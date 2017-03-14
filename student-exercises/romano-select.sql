ddbc8fa79a7608f131687c5971143e9941d98326
Lawrence Romano <l.j.romano@gmail.com>
2017-03-14 11:33:25 -0400

    Added select queries

---
+++ student-exercises/romano-select.sql
@@ -0,0 +1,15 @@
+USE rockStarDay2;
+
+/*
+SELECT * FROM individual;
+
+SELECT * FROM individual WHERE deceasedDate IS NULL;
+
+SELECT DISTINCT genre FROM band;
+
+SELECT * FROM band WHERE genre = 'Country' ORDER BY yearFormed DESC;
+*/
+
+SHOW Columns from band;
+
+SELECT ID, name, genre from band;
